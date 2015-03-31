f137a16c4f306cff5fe09e9bfce41f1ac10e2aa0
Trey Evans <lewis.r.evans@gmail.com>
2015-03-31 12:55:28 -0400

    Now can burn.

--- app/assets/javascripts/bucket.js
+++ app/assets/javascripts/bucket.js
@@ -170,66 +170,6 @@ function submitPaste() {
     });
 }
 
-/**
- *  Send a new paste to server
- */
-function send_data() {
-	// Do not send if no data.
-	if ($('textarea#message').val().length == 0) {
-		return;
-	}
-
-	// If sjcl has not collected enough entropy yet, display a message.
-	if (!sjcl.random.isReady())
-	{
-		showStatus('Sending paste (Please move your mouse for more entropy)...', spin=true);
-		sjcl.random.addEventListener('seeded', function(){ send_data(); }); 
-		return; 
-	}
-
-	showStatus('Sending paste...', spin=true);
-
-	var randomkey = sjcl.codec.base64.fromBits(sjcl.random.randomWords(8, 0), 0);
-	var cipherdata = zeroCipher(randomkey, $('textarea#message').val());
-	var data_to_send = { data:           cipherdata,
-		expire:         $('select#pasteExpiration').val(),
-		burnafterreading: $('input#burnafterreading').is(':checked') ? 1 : 0,
-		opendiscussion: $('input#opendiscussion').is(':checked') ? 1 : 0,
-		syntaxcoloring: $('input#syntaxcoloring').is(':checked') ? 1 : 0
-	};
-	$.post(postLocation(), data_to_send, 'json')
-		.error(function() {
-			showError('Data could not be sent (serveur error or not responding).');
-		})
-	.success(function(data) {
-		if (data.status == 0) {
-			stateExistingPaste();
-			var url = postLocation() + "?" + data.id + '#' + randomkey;
-			var deleteUrl = postLocation() + "?pasteid=" + data.id + '&deletetoken=' + data.deletetoken;
-			showStatus('');
-
-			$('div#pastelink').html('Your paste is <a id="pasteurl" href="' + url + '">' + url + '</a> <span id="copyhint">(Hit CTRL+C to copy)</span>');
-			$('div#deletelink').html('<a href="' + deleteUrl + '">Delete link</a>');
-			$('div#pasteresult').show();
-			selectText('pasteurl'); // We pre-select the link so that the user only has to CTRL+C the link.
-
-			setElementText($('div#cleartext'), $('textarea#message').val());
-			urls2links($('div#cleartext'));
-
-			// FIXME: Add option to remove syntax highlighting ?
-			if ($('input#syntaxcoloring').is(':checked')) applySyntaxColoring();
-
-			showStatus('');
-		}
-		else if (data.status==1) {
-			showError('Could not create paste: '+data.message);
-		}
-		else {
-			showError('Could not create paste.');
-		}
-	});
-}
-
 /** Return raw text
 */
 function rawText()
@@ -301,6 +241,8 @@ $(function() {
           var cipher_obj = JSON.parse(ciphertext);
 	  try { // Try to decrypt the paste.
 	    $("#decoded").text(zeroDecipher(pageKey(), cipher_obj));
+            var burnkey_value = sjcl.codec.base64.fromBits(sjcl.hash.sha256.hash(compress($('#decoded').text())));
+	    $("#burnkey").val(burnkey_value);
             $("#status").hide();
 	  } catch(err) {
 	    showError('Could not decrypt data (Wrong key ?)');
--- app/controllers/pastes_controller.rb
+++ app/controllers/pastes_controller.rb
@@ -1,4 +1,10 @@
 class PastesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
+  def record_not_found
+    redirect_to new_paste_path
+  end
+
   def new
     @paste = Paste.new
   end
@@ -17,5 +23,15 @@ class PastesController < ApplicationController
   end
 
   def burn
+    @paste = Paste.find(params.permit(:id)[:id])
+
+    burnkey = params.permit(:paste => [:burnkey])[:paste][:burnkey]
+
+    if @paste.burnkey == burnkey
+      @paste.destroy!
+      redirect_to new_paste_path
+    else
+      redirect_to @paste
+    end
   end
 end
